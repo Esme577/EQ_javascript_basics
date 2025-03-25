ce3861efe3ec8e8c53e17d3a42145eb581922255
Esme577 <esmeraldaquezada57@gmail.com>
2025-03-24 19:35:07 -0700

    added java and htm to the 2files

---
+++ script.js
@@ -0,0 +1,17 @@
+let article_1 = {
+    tittle: 'Why Accesibility Matters in UI UX Design',
+    date: 'May 25, 2023',
+    description: 'Accessibility is a word that is often thrown around in the design field. As a UI?UX Web Designer. you might have heard this word a few times and you might wonder why its important.'
+}
+document.getElementById("title").innerHTML = article_1.tittle;
+document.getElementById("date").innerHTML = article_1.date;
+document.getElementById("description").innerHTML = article_1.description;
+
+let article_2 = {
+    tittle: 'When where unicorns discovered',
+    date: 'May 25, 2023',
+    description: 'Unicorns are from a reomte place far from earth. That got stuck on a visit or are they exstinct.whos knows?'
+}
+document.getElementById("title2").innerHTML = article_2.tittle;
+document.getElementById("date2").innerHTML = article_2.date;
+document.getElementById("description2").innerHTML = article_2.description;
