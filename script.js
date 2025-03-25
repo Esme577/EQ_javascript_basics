5599c1e4fbeb457130d1f9b4063da6dec475afe5
Esme577 <esmeraldaquezada57@gmail.com>
2025-03-24 20:15:42 -0700

    the lessoon that im getting for today is that const is a variable that does not change but let and var can but its more commonly used is let as its not so complicated as var to use

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ let article_2 = {
 document.getElementById("title2").innerHTML = article_2.tittle;
 document.getElementById("date2").innerHTML = article_2.date;
 document.getElementById("description2").innerHTML = article_2.description;
+
+
+let article_3 = {
+    tittle: 'let var and const explained for todays lesson',
+    date: 'May 25, 2023',
+    description: 'When are doing this so that we explain what let var, const are and how we access the java script we link our java script as the following <script src="script.js"></script> from that we in our body crete our container with each having its id for tittle date  and descriptiwhat is actually written out in our page is whats on our java script and in this case we use let and article and we called uppon each article we wanted displayed byb its id.'
+}
+document.getElementById("title3").innerHTML = article_3.tittle;
+document.getElementById("date3").innerHTML = article_3.date;
+document.getElementById("description3").innerHTML = article_3.description;
